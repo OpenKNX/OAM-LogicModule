4fba4a099cdd098030ec9a940f1e02a62768c6e1
Waldemar Porscha <waldemar.porscha@sap.com>
2022-04-11 09:34:01 +0200

    Release preparation - update scripts - releases are not selfcontained anymore

--- scripts/Build-Release.ps1
+++ scripts/Build-Release.ps1
@@ -7,23 +7,30 @@ if (Test-Path -Path release) {
 }
 
 # create required directories
-New-Item -Path release/tools -ItemType Directory | Out-Null
 New-Item -Path release/data -ItemType Directory | Out-Null
-
-# add necessary executables - will be moved to an extra package in future
-Copy-Item ~/bin/OpenKNXproducer.exe release/tools/
-Copy-Item ~/bin/bossac.exe release/tools/
 
 # get xml for kxnprod
 ~/bin/OpenKNXproducer.exe create --Debug --Output=release/LogicModule.knxprod --HeaderFileName=src/Logikmodul.h src/Logikmodul-Release.xml
+if (!$?) {
+    Write-Host "Error in knxprod, Release was not built!"
+    exit 1
+}
 Move-Item src/Logikmodul-Release.debug.xml release/data/LogicModule.xml
 
 # build firmware based on generated headerfile for PICO
 ~/.platformio/penv/Scripts/pio.exe run -e build_RP2040
+if (!$?) {
+    Write-Host "RP2040 build failed, Release was not built!"
+    exit 1
+}
 Copy-Item .pio/build/build_RP2040/firmware.uf2 release/data/
 
 # build firmware based on generated headerfile for SAMD
 ~/.platformio/penv/Scripts/pio.exe run -e build_SAMD
+if (!$?) {
+    Write-Host "SAMD build failed, Release was not built!"
+    exit 1
+}
 Copy-Item .pio/build/build_SAMD/firmware.bin release/data/
 
 # add necessary scripts
@@ -37,4 +44,6 @@ Remove-Item release/LogicModule.knxprod
 # create package 
 Compress-Archive -Path release/* -DestinationPath Release.zip
 Remove-Item -Recurse release/*
-Move-Item Release.zip release/
+Move-Item Release.zip release/LogicModule.zip
+
+Write-Host "Release successfully created!"
--- scripts/Upload-Firmware-SAMD.ps1
+++ scripts/Upload-Firmware-SAMD.ps1
@@ -1,27 +1,34 @@
-Write-Host Suche COM-Port fuer SAMD...
-$portList = get-pnpdevice -class Ports
-if ($portList) {
-    foreach($device in $portList) {
-        if ($device.Present) {
-            $isCom = $device.Name -match "COM(\d{1,3})"
-            if($isCom)
-            {
-                Write-Host Gefunden $port
-                $port = $Matches[0]
-                break
+$toolsExist = Test-Path -PathType Leaf ~/bin/bossac.exe
+if (!$toolsExist) {
+    ./Build-knxprod.ps1
+}
+
+if ($toolsExist) {
+    Write-Host Suche COM-Port fuer SAMD...
+    $portList = get-pnpdevice -class Ports
+    if ($portList) {
+        foreach($device in $portList) {
+            if ($device.Present) {
+                $isCom = $device.Name -match "COM(\d{1,3})"
+                if($isCom)
+                {
+                    Write-Host Gefunden $port
+                    $port = $Matches[0]
+                    break
+                }
             }
         }
+        if($port)
+        {
+            Write-Host Verwende $port
+            $serial = new-Object System.IO.Ports.SerialPort $port,1200,None,8,1
+            $serial.Open()
+            $serial.Close()
+            Start-Sleep -s 1
+            ~/bin/bossac --info --write --verify --reset --erase data/firmware.bin
+        } else {
+            Write-Host Kein Port gefunden!
+        }
     }
-    if($port)
-    {
-        Write-Host Verwende $port
-        $serial = new-Object System.IO.Ports.SerialPort $port,1200,None,8,1
-        $serial.Open()
-        $serial.Close()
-        Start-Sleep -s 1
-        tools/bossac --info --write --verify --reset --erase data/firmware.bin
-    } else {
-       Write-Host Kein Port gefunden!
-    }
+    timeout /T 20 
 }
-timeout /T 20 
