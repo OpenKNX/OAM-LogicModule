7e4509644430a305630bba832e00ba75210c89f1
Waldemar Porscha <waldemar.porscha@sap.com>
2022-05-15 15:14:47 +0200

    just typos and reformatting

--- src/KnxHelper.cpp
+++ src/KnxHelper.cpp
@@ -1,6 +1,6 @@
 #include "KnxHelper.h"
 
-static Dpt sDpt[] = {Dpt(1, 1), Dpt(2, 1), Dpt(5, 10), Dpt(5, 1), Dpt(6, 1), Dpt(7, 1), Dpt(8, 1), Dpt(9, 2), Dpt(16, 1), Dpt(17, 1), Dpt(232, 600), Dpt(10,1,1), Dpt(11,1), Dpt(12,1)};
+static Dpt sDpt[] = {Dpt(1, 1), Dpt(2, 1), Dpt(5, 10), Dpt(5, 1), Dpt(6, 1), Dpt(7, 1), Dpt(8, 1), Dpt(9, 2), Dpt(16, 1), Dpt(17, 1), Dpt(232, 600), Dpt(10, 1, 1), Dpt(11, 1), Dpt(12, 1)};
 
 Dpt &getDPT(uint8_t iDptIndex)
 {
@@ -16,7 +16,7 @@ Dpt &getDPT(uint8_t iDptIndex)
  *   10 - hours
  *   11 - seconds/10
  * 
- * remainig 14 bit: uint14
+ * remaining 14 bit: uint14
 */
 uint32_t getDelayPattern(uint16_t iParamIndex, bool iAsSeconds /* = false */) {
 
