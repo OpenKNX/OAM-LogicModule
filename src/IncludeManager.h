01ae74cec217d1cac6450d83f4ab17fbdb188b06
Waldemar Porscha <wp@porscha.eu>
2022-09-14 21:55:51 +0200

    Infrastructure changes - new micro scheduler added - added SensorVpmModule to IncludeManager - Moved types from Sensor to Logik for better reuse

--- src/IncludeManager.h
+++ src/IncludeManager.h
@@ -3,27 +3,35 @@
 #define XSTR(x) STR(x)
 
 #ifdef LOGICMODULE
-#pragma message "Building Logic for LOGICMODULE"
+#pragma message "Building Logic for LOGIC-MODULE"
 #include "Logikmodul.h"
 #endif
 #ifdef SENSORMODULE
-#pragma message "Building Logic for SENSORMODULE"
+#pragma message "Building Logic for SENSOR-MODULE"
 #include "../../OAM-SensorModule/src/Sensormodul.h"
+#define WIREMODULE 1
 #endif
 #ifdef PMMODULE
-#pragma message "Building Logic for PMMODULE"
+#pragma message "Building Logic for PM-MODULE"
 #include "../../OAM-PresenceModule/src/PMmodul.h"
 #endif
+#ifdef SENSORVPMMODULE
+#pragma message "Building Logic for SENSOR-VPM-MODULE"
+#include "../../OAM-SensorVpmModule/src/SensormodulVpm.h"
+#define SENSORMODULE 1
+#define PMMOULE 1
+#endif
 #ifdef WIREGATEWAY
-#pragma message "Building Logic for WIREGATEWAY"
+#pragma message "Building Logic for WIRE-GATEWAY"
 #include "../../OAM-OneWireModule/src/WireGateway.h"
+#define WIREMODULE 1
 #endif
 #ifdef ENOCEANGATEWAY
-#pragma message "Building Logic for ENOCEANGATEWAY"
+#pragma message "Building Logic for ENOCEAN-GATEWAY"
 #include "../../OAM-EnoceanGateway/src/EnoceanGateway.h"
 #endif
 #ifdef GARDENCONTROL
-#pragma message "Building Logic for GARDENCONTROL"
+#pragma message "Building Logic for GARDEN-CONTROL"
 #include "../../BEM-GardenControl/src/GardenControl.h"
 #endif
 // #ifdef TEST
