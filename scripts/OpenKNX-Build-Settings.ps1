123d5916cfd931a3ed7c5d35891b283785a1eebb
Waldemar Porscha <wp@porscha.eu>
2022-11-27 12:19:04 +0100

    aligned path in all setup scripts

--- scripts/OpenKNX-Build-Settings.ps1
+++ scripts/OpenKNX-Build-Settings.ps1
@@ -1,10 +1,5 @@
 
 # set product names, allows mapping of (devel) name in Project to a more consistent name in release
-$settings = lib/OGM-Common/build-scripts/OpenKNX-Build-Settings.ps1 $args[0] "Logikmodul" "LogicModule"
-
-# $settings.sourceName="Logikmodul"  
-# $settings.targetName="LogicModule" 
-# $settings.knxprod="src/{0}.h" -f $settings.sourceName
-# $settings.hardware="src/{0}Hardware.h" -f $settings.sourceName
+$settings = lib/OGM-Common/scripts/build/OpenKNX-Build-Settings.ps1 $args[0] "Logikmodul" "LogicModule"
 
 Return $settings
