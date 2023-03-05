4c9854a86cc19e24497488a6d182ae27b028eebe
Waldemar Porscha <wp@porscha.eu>
2023-03-05 22:11:20 +0100

    Split to OAM and OFM

--- setup/initialSetup.bat
+++
@@ -1,9 +0,0 @@
-REM Under construction, do NOT use
-
-mkdir %1
-cd %1
-git clone https://github.com/thelsing/knx
-git clone https://github.com/OpenKNX/OGM-Common
-git clone https://github.com/OpenKNX/OGM-SensorDevices
-git clone https://github.com/OpenKNX/OAM-LogicModule
-code OAM-LogicModule\LogicModule.code-workspace
