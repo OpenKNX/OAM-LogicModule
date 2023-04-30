1d1080e63c370d6aedbf408420255d3c5d48b55c
Waldemar Porscha <wp@porscha.eu>
2023-04-30 14:30:05 +0200

    feat: Add Update via KNX-Bus in release
    - Add build-postprocess.py
    - Update dependencies.txt
    - Modify gzip-bin.py to import zlib instead of using max
    - Modify platformio.ini to add flags for faster compilation
    - Add Get-Version-Information.ps1 to scripts directory

--- build-postprocess.py
+++ build-postprocess.py
@@ -1,6 +1,6 @@
 Import("env")
 import gzip
-import subprocess, sys
+import re
 
 def post_program_action(source, target, env):
     print("Building ", source[0].get_path() + ".gz")
@@ -10,15 +10,21 @@ def post_program_action(source, target, env):
 
 def post_program_uf2(source, target, env):
     print("Building ", source[0].get_path()[0:-4] + "_new.uf2")
-    p = subprocess.Popen(["powershell.exe", 
-                "(scripts/Get-Version-Information.ps1).UF2Version"], 
-                stdout=subprocess.PIPE)
-    (uf2Version, err) = p.communicate()
-    uf2Version = uf2Version.decode('ascii').split(",")
-    openKnxId = uf2Version[0]
-    appNumber = uf2Version[1]
-    appVersion = uf2Version[2]
-    appRevision = uf2Version[3]
+    with open(env["PROJECT_DIR"] + "/../OGM-Common/include/knxprod.h", 'r') as knxprod:
+        content = knxprod.read(-1)
+
+    m = re.search("#define MAIN_OpenKnxId 0x([0-9A-Fa-f]{2})", content)
+    openKnxId = m.group(1)
+    m = re.search("#define MAIN_ApplicationNumber ([0-9A-Fa-f]{2})", content)
+    appNumber = m.group(1)
+    m = re.search("#define MAIN_ApplicationVersion ([0-9A-Fa-f]{2})", content)
+    appVersion = m.group(1)
+    
+    with open(env["PROJECT_SRC_DIR"] + "/main.cpp", 'r') as knxprod:
+        content = knxprod.read(-1)
+
+    m = re.search("firmwareRevision = ([0-9]+);", content)
+    appRevision = m.group(1)    
     print("Id: " + openKnxId + ", Number: " + appNumber + ", Version: " + appVersion + ", Revision: " + appRevision)
     with open(source[0].get_path()[0:-4] + ".uf2", "rb") as orig_file:
         barray=bytearray(orig_file.read())
@@ -27,7 +33,7 @@ def post_program_uf2(source, target, env):
         barray[289] = ord('K') #Type
         barray[290] = ord('N') #Type
         barray[291] = ord('X') #Type
-        barray[292] = int(openKnxId) #Data
+        barray[292] = int(openKnxId, 16) #Data
         barray[293] = int(appNumber) #Data
         barray[294] = int(appVersion) #Data
         barray[295] = int(appRevision) #Data
