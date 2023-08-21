98028f175833bce079e03ffd9e281aaf0d9a4701
Waldemar Porscha <wp@porscha.eu>
2023-08-21 13:06:42 +0200

    Prepare-1.5

--- include/versions.h
+++ include/versions.h
@@ -1,7 +1,6 @@
 #pragma once
 
-#define MAIN_Version "88d7030"
+#define MAIN_Version "9c9bb1e"
 #define KNX_Version "6dda682"
-#define MODULE_LogicModule_Version "5.0.0+9c073b9"
+#define MODULE_LogicModule_Version "5.0.0+7daa463"
 #define MODULE_Common_Version "1.0.0+2bdd764"
-#define MODULE_Updater_Version "0.0.0+0fa5d39"
