9c9bb1e1a0f472af38af8b46b7bd1001413bd92a
Waldemar Porscha <wp@porscha.eu>
2023-08-21 08:48:27 +0200

    OFM-Versioning with ModuleVersion.h

--- include/versions.h
+++ include/versions.h
@@ -1,6 +1,7 @@
 #pragma once
 
-#define MAIN_Version "ade0f02"
+#define MAIN_Version "88d7030"
 #define KNX_Version "6dda682"
-#define MODULE_LogicModule_Version "5.0.0+baf583b"
+#define MODULE_LogicModule_Version "5.0.0+9c073b9"
 #define MODULE_Common_Version "1.0.0+2bdd764"
+#define MODULE_Updater_Version "0.0.0+0fa5d39"
