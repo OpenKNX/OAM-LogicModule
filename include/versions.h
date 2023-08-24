3484adee2835c42cd735ce10b98261bf81b06412
Waldemar Porscha <wp@porscha.eu>
2023-08-24 12:18:55 +0200

    update documentation

--- include/versions.h
+++ include/versions.h
@@ -1,6 +1,7 @@
 #pragma once
 
-#define MAIN_Version "9c9bb1e"
-#define KNX_Version "6dda682"
+#define MAIN_Version "98028f1"
+#define KNX_Version "9c4e5d2"
 #define MODULE_LogicModule_Version "5.0.0+7daa463"
 #define MODULE_Common_Version "1.0.0+2bdd764"
+#define MODULE_Updater_Version "0.0.0+0fa5d39"
