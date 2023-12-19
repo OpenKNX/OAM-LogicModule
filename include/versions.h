8b659c06bcfcdcdc025f5aafb195f0cf608a7ce1
Waldemar Porscha <wp@porscha.eu>
2023-12-19 23:59:19 +0100

    Update dependencies.txt

--- include/versions.h
+++ include/versions.h
@@ -1,10 +1,10 @@
 #pragma once
 
-#define MAIN_Version "3ccf5d6"
+#define MAIN_Version "f03f53b"
 #define KNX_Version "9db2cd5"
 #define MODULE_LogicModule_Version "3.0.0+3559187"
 #define MODULE_LogicModule_ETS 48
-#define MODULE_Common_Version "1.0.9+a49ed72"
+#define MODULE_Common_Version "1.0.9+944e0da"
 #define MODULE_Common_ETS 16
 #define MODULE_FileTransferModule_Version "0.0.4+309dd74"
 #define MODULE_FileTransferModule_ETS 0
