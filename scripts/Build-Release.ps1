ecc9f042f6a6ff36747d472aa300e6527e800b50
Erkan Çolak <erkanc@gmx.de>
2023-12-20 20:39:44 +0100

    Revamp: 🚧 Work in progress - build processes

--- scripts/Build-Release.ps1
+++ scripts/Build-Release.ps1
@@ -1,68 +1,23 @@
-# This script is just a template and has to be copied and modified per project
-# This script should be called from .vscode/tasks.json with
-#
-#   scripts/Build-Release.ps1            - for Beta builds
-#   scripts/Build-Release.ps1 Release    - for Release builds
-#
-# {
-#     "label": "Build-Release",
-#     "type": "shell",
-#     "command": "scripts/Build-Release.ps1 Release",
-#     "args": [],
-#     "problemMatcher": [],
-#     "group": "test"
-# },
-# {
-#     "label": "Build-Beta",
-#     "type": "shell",
-#     "command": "scripts/Build-Release.ps1 ",
-#     "args": [],
-#     "problemMatcher": [],
-#     "group": "test"
-# }
 
+# Define target hardware
+$opknxhw = @(
+    @{Target_Ini= "platformio.custom.ini"; OAM_Support_ToMatch = "oam_logicmodule" } 
+)
 
+# Define firmware build information
+$builds = @(
+    @{Release="release_PiPico_BCU_Connector"; Firmware="firmware-PiPico-BCU-Connector"; Type="uf2"},
+    @{Release="release_1TE_RP2040_SmartMF"; Firmware="firmware-1TE-RP2040-SmartMF"; Type="uf2"},
+    @{Release="release_OpenKNX_REG1_Controller2040"; Firmware="firmware-OpenKNX-REG1-BASE-V0"; Type="uf2"},
+    @{Release="release_OpenKNX_REG1_BASE_V1"; Firmware="firmware-OpenKNX-REG1-BASE-V1"; Type="uf2"},
+    @{Release="release_Sensormodul_Breakout_RP2040"; Firmware="firmware-Sensormodul-Breakout-RP2040"; Type="uf2"; Extra="Sensormodul-Breakout-Board-Just-for-testers"},
+    @{Release="release_Sensormodul_v40_RP2040"; Firmware="firmware-Sensormodul-v40-RP2040"; Type="uf2"},
+    @{Release="release_Sensormodul_v31_SAMD"; Firmware="firmware-Sensormodul-v31-SAMD"; Type="bin"},
+    @{Release="release_Sensormodul_v30_SAMD"; Firmware="firmware-Sensormodul-v30-SAMD"; Type="bin"},
+    @{Release="release_OpenKNX_REG2_PiPico"; Firmware="firmware-OpenKNX-REG2-PiPico"; Type="uf2"},
+    @{Release="release_OKNXHW_REG2_PiPico_W"; Firmware="firmware-OpenKNX-REG2-PiPico_W"; Type="uf2"}
+)
 
-# set product names, allows mapping of (devel) name in Project to a more consistent name in release
-# $settings = scripts/OpenKNX-Build-Settings.ps1
-
-# execute generic pre-build steps
-lib/OGM-Common/scripts/setup/reusable/Build-Release-Preprocess.ps1 $args[0]
-if (!$?) { exit 1 }
-
-# build firmware based on generated headerfile 
-
-# build firmware for PiPico-BCU-Connector
-lib/OGM-Common/scripts/setup/reusable/Build-Step.ps1 release_PiPico_BCU_Connector firmware-PiPico-BCU-Connector uf2
-if (!$?) { exit 1 }
-
-# build firmware for 1TE-RP2040-SmartMF
-lib/OGM-Common/scripts/setup/reusable/Build-Step.ps1 release_1TE_RP2040_SmartMF firmware-1TE-RP2040-SmartMF uf2
-if (!$?) { exit 1 }
-
-# build firmware for OpenKNX-REG1-Base
-lib/OGM-Common/scripts/setup/reusable/Build-Step.ps1 release_OpenKNX_REG1_Controller2040 firmware-OpenKNX-REG1-BASE-V0 uf2
-if (!$?) { exit 1 }
-
-# build firmware for OpenKNX-REG1-Base
-lib/OGM-Common/scripts/setup/reusable/Build-Step.ps1 release_OpenKNX_REG1_BASE_V1 firmware-OpenKNX-REG1-BASE-V1 uf2
-if (!$?) { exit 1 }
-
-# build firmware for RP2040 sensormodule breakout board
-lib/OGM-Common/scripts/setup/reusable/Build-Step.ps1 release_Sensormodul_Breakout_RP2040 firmware-Sensormodul-Breakout-RP2040 uf2 Sensormodul-Breakout-Board-Just-for-testers
-if (!$?) { exit 1 }
-
-# build firmware for RP2040 sensormodule 
-lib/OGM-Common/scripts/setup/reusable/Build-Step.ps1 release_Sensormodul_v40_RP2040 firmware-Sensormodul-v40-RP2040 uf2
-if (!$?) { exit 1 }
-
-# build firmware based on generated headerfile for SAMD
-# lib/OGM-Common/scripts/setup/reusable/Build-Step.ps1 release_Sensormodul_v31_SAMD firmware-Sensormodul-v31-SAMD bin
-# if (!$?) { exit 1 }
-
-# lib/OGM-Common/scripts/setup/reusable/Build-Step.ps1 release_Sensormodul_v30_SAMD firmware-Sensormodul-v30-SAMD bin
-# if (!$?) { exit 1 }
-
-# execute generic post-build steps
-lib/OGM-Common/scripts/setup/reusable/Build-Release-Postprocess.ps1 $args[0]
+# Build firmware for each defined build
+lib/OGM-Common/scripts/setup/reusable/Build-Release.ps1 $args[0] $builds $opknxhw
 if (!$?) { exit 1 }
