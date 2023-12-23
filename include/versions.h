304d76903f4e246b2090c51cb105e934277d9e27
Waldemar Porscha <wp@porscha.eu>
2023-12-23 12:06:18 +0100

    Prepare 3.0:
    - Updated Restore-Dependencies

--- include/versions.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define MAIN_Version "8b659c0"
-#define KNX_Version "e272be7"
-#define MODULE_LogicModule_Version "3.0.0+91c9d8c"
-#define MODULE_LogicModule_ETS 48
-#define MODULE_Common_Version "1.0.10+2f282aa"
