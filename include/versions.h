00c38559fc1a4172bbdf2ce588fae83b42ec5bb5
Waldemar Porscha <wp@porscha.eu>
2023-12-24 12:05:54 +0100

    RC Logik 3.0:
    - added library.json
    - updated revision
    - cleanup some files

--- include/versions.h
+++ include/versions.h
@@ -1,9 +1,7 @@
 #pragma once
 
-#define MAIN_Version "bf29515"
+#define MAIN_Version "9205ba3"
 #define KNX_Version "e272be7"
 #define MODULE_LogicModule_Version "3.0.0+91c9d8c"
 #define MODULE_LogicModule_ETS 48
 #define MODULE_Common_Version "1.0.10+2f282aa"
-#define MODULE_FileTransferModule_Version "0.0.4+19f6f21"
-#define MODULE_FileTransferModule_ETS 0
